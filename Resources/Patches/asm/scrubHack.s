977456f317ce97a2f9618269b54c53ce077ec8fc
LeonarthCG <leonarth.cg@gmail.com>
2019-08-09 11:32:55 +0200

    made the scrubs (that only sell once) work as spots
    in order to change the item they sell, the respective definitions need to be defined in "extDefinitions.event", which is by default an empty file
    
    this file is meant for the randomizer to be able to send information to EA
    
    if not defined, the scrubs will simply sell their original items

---
+++ Resources/Patches/asm/scrubHack.s
@@ -0,0 +1,34 @@
+.equ return, table+4
+.thumb
+push	{r1}
+ldr	r0,table
+loop:
+ldr	r1,[r0]
+cmp	r1,#0
+beq	vanilla
+cmp	r1,r6
+beq	match
+add	r0,#8
+b	loop
+
+match:
+pop	{r1}
+ldrh	r1,[r0,#6]
+ldrh	r0,[r0,#4]
+b	end
+
+vanilla:
+ldrb	r0,[r6,#8]
+pop	{r1}
+
+end:
+mov	r2,#0
+ldr	r3,=#0x80A73F8
+mov	lr,r3
+.short	0xF800
+ldr	r3,return
+bx	r3
+
+.align
+.ltorg
+table:
