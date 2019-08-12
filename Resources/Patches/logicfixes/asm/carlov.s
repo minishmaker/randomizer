9cbacf6673b7d78b365a07a10692e0eb47df77b5
LeonarthCG <leonarth.cg@gmail.com>
2019-08-12 22:07:57 +0200

    fix simulation

--- Resources/Patches/logicfixes/asm/carlov.s
+++ Resources/Patches/logicfixes/asm/carlov.s
@@ -1,9 +1,15 @@
 .equ sub, item+4
 .thumb
 push	{r4,lr}
-mov	r4,r0
-mov	r1,#0
-str	r1,[r4,#20]
+mov	r4,r1
+ldr	r0,[r4]
+ldr	r3,=#0x80169CE
+mov	lr,r3
+.short	0xF800
+ldr	r3,=#0x807C654
+mov	lr,r3
+.short	0xF800
+str	r0,[r4,#20]
 
 @check if area and room are correct
 ldr	r0,=#0x3000BF0
@@ -15,6 +21,9 @@ mov	r1,#0x07
 ldrb	r2,[r0,#5]
 cmp	r2,r1
 bne	end
+
+mov	r0,#4
+str	r0,[r4,#20]
 
 @check if custom flag is set
 ldr	r0,=#0x2002EA4
