f445ee56b16fe47589e027c65d382b81efd2a3bd
wjg <torak2@live.nl>
2019-08-12 19:34:53 +0200

    fix most items showing as UnTyped
    Changed 1 & 2 to 1 | 2.
    Added item override to scrubs as the program has no clue where to pull the value from.

--- Randomizer/LoacationAddress.cs
+++ Randomizer/LoacationAddress.cs
@@ -15,7 +15,7 @@ namespace MinishRandomizer.Randomizer
         None = 0,
         FirstByte = 1,
         SecondByte = 2,
-        BothBytes = FirstByte & SecondByte,
+        BothBytes = FirstByte | SecondByte,
         GroundItem = 4,
         Define = 8
     }
