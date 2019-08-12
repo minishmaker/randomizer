74496168acb2bd420d44927f711b58bba04b42ce
LeonarthCG <leonarth.cg@gmail.com>
2019-08-12 13:42:17 +0200

    bell item spot now looks like vanilla

--- Resources/Patches/logicfixes/asm/bellHeart.s
+++ Resources/Patches/logicfixes/asm/bellHeart.s
@@ -2,13 +2,9 @@
 ldr	r3,=#0x80A217C
 mov	lr,r3
 .short	0xF800
-ldr	r3,=#0x2002CD5
-mov	r2,#0x20
-ldrb	r1,[r3]
-orr	r1,r2
-strb	r1,[r3]
-mov	r1,#1
-strb	r1,[r0,#0x0E]
+mov	r2,#0x86
+mov	r3,#0xCD
+strh	r3,[r0,r2]
 mov	r1,r0
 cmp	r1,#0
 beq	goto805D620
