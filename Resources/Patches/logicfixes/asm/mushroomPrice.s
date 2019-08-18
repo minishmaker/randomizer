3cab43ac96f647b4665f2a34d5889eb024932e39
LeonarthCG <leonarth.cg@gmail.com>
2019-08-18 22:59:31 +0200

    fix bread shop prices

--- Resources/Patches/logicfixes/asm/mushroomPrice.s
+++ Resources/Patches/logicfixes/asm/mushroomPrice.s
@@ -1,11 +1,27 @@
 .equ bluePrice, spotPrice+4
 .equ redPrice, bluePrice+4
+.equ briochePrice, redPrice+4
+.equ croissantPrice, briochePrice+4
+.equ piePrice, croissantPrice+4
+.equ cakePrice, piePrice+4
 .thumb
+@check if breads
+cmp	r4,#0x68
+beq	brioche
+cmp	r4,#0x69
+beq	croissant
+cmp	r4,#0x6A
+beq	pie
+cmp	r4,#0x6B
+beq	cake
+
+@check if potions
 cmp	r4,#0x24
 beq	red
 cmp	r4,#0x25
 beq	blue
 
+@otherwise, mushroom spot
 other:
 ldr	r2,spotPrice
 b	end
@@ -16,6 +32,23 @@ b	end
 
 red:
 ldr	r2,redPrice
+b	end
+
+brioche:
+ldr	r2,briochePrice
+b	end
+
+croissant:
+ldr	r2,croissantPrice
+b	end
+
+pie:
+ldr	r2,piePrice
+b	end
+
+cake:
+ldr	r2,cakePrice
+b	end
 
 end:
 ldr	r3,=#0x807DE6D
