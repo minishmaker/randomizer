0b26a73dfa6564cf873bf4ae032b035b30ef44d3
LeonarthCG <leonarth.cg@gmail.com>
2019-08-18 17:51:06 +0200

    fix book graphics

--- Resources/Patches/asm/fixBookGraphics.s
+++ Resources/Patches/asm/fixBookGraphics.s
@@ -1,17 +1,11 @@
 .thumb
+push	{r4,r5,lr}
+mov	r4,r0
 ldrb	r0,[r4,#0x0A]
-ldr	r1,table
-mov	r2,#12
-mul	r0,r2
-ldrh	r0,[r1,r0]
-
+mov	r3,#1
+strb	r3,[r4,#0x18]
 ldr	r3,=#0x807C4A8
 mov	lr,r3
 .short	0xF800
-
 ldr	r3,=#0x809AC61
 bx	r3
-
-.align
-.ltorg
-table:
