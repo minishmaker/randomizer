2f21d607e0202b7682dca566f5764a015e13ad23
LeonarthCG <leonarth.cg@gmail.com>
2019-08-20 06:24:10 +0200

    actually fix clouds

--- Resources/Patches/gamefixes/asm/cloudFix.s
+++ Resources/Patches/gamefixes/asm/cloudFix.s
@@ -13,9 +13,16 @@ cmp	r0,#4
 bne	end
 
 @set transparency
-ldr	r0,=#0x3000FB8
-ldr	r1,=#0x0E04
+ldr	r0,=#0x3000FB4
+mov	r1,#0
 strh	r1,[r0]
+ldr	r1,=#0x3648
+strh	r1,[r0,#2]
+ldr	r1,=#0x0E04
+strh	r1,[r0,#4]
+ldr	r0,=#0x3001000
+mov	r1,#0
+strb	r1,[r0]
 
 end:
 cmp	r4,#0
