dbe395ef23f3e63b6ac4d2b310651298de6299ea
LeonarthCG <leonarth.cg@gmail.com>
2019-08-28 04:23:54 +0200

    time of first kill of each enemy type

--- Resources/Patches/save/asm/killCounter.s
+++ Resources/Patches/save/asm/killCounter.s
@@ -1,4 +1,5 @@
 .equ table, offset+4
+.equ time, table+4
 .thumb
 ldr	r0,[r1,#0x50]
 mov	r7,#1
@@ -20,6 +21,16 @@ cmp	r1,#0xFF
 beq	end
 ldr	r2,offset
 ldrh	r0,[r2,r1]
+cmp	r0,#0
+bne	notFirst
+push	{r0-r3}
+ldr	r0,time
+lsl	r1,#2
+ldr	r2,=#0x203FFF0
+ldr	r2,[r2]
+str	r2,[r0,r1]
+pop	{r0-r3}
+notFirst:
 ldr	r3,=#10000
 cmp	r0,r3
 beq	end
