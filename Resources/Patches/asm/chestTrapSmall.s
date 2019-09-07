a8528ae378af8e9f318d09368435ee77d06bc339
LeonarthCG <leonarth.cg@gmail.com>
2019-09-08 00:17:57 +0200

    added zap trap, added damage to traps

--- Resources/Patches/asm/chestTrapSmall.s
+++ Resources/Patches/asm/chestTrapSmall.s
@@ -10,11 +10,20 @@ b	end
 
 isTrap:
 ldr	r0,table
-lsl	r1,#2
+lsl	r1,#3
+push	{r0,r1}
 ldr	r0,[r0,r1]
 cmp	r0,#0
 beq	end
 mov	lr,r0
+.short	0xF800
+pop	{r0,r1}
+add	r1,r0
+ldr	r1,[r0,#4]
+mov	r0,#0
+sub	r0,r1
+ldr	r3,=#0x80522BC
+mov	lr,r3
 .short	0xF800
 
 mov	r0,#0x74
