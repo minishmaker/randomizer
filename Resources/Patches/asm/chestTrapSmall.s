a6bae5ba35dcbfb75f98931e75d1c7d116642cd1
LeonarthCG <leonarth.cg@gmail.com>
2019-09-07 22:15:09 +0200

    added traps (ice chest)

---
+++ Resources/Patches/asm/chestTrapSmall.s
@@ -0,0 +1,25 @@
+.thumb
+ldrb	r1,[r4,#3]
+mov	r2,#0
+cmp	r0,#0x1B
+beq	isTrap
+ldr	r3,=#0x80A73F8
+mov	lr,r3
+.short	0xF800
+b	end
+
+isTrap:
+ldr	r0,table
+lsl	r1,#2
+ldr	r0,[r0,r1]
+cmp	r0,#0
+beq	end
+mov	lr,r0
+.short	0xF800
+
+end:
+ldr	r3,=#0x80A74D5
+bx	r3
+.align
+.ltorg
+table:
