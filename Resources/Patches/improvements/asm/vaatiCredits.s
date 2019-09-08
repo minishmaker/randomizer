5aadf8cbaa02faa1de70a390778cd67fe3216afc
LeonarthCG <leonarth.cg@gmail.com>
2019-09-08 03:31:18 +0200

    fix vaati kill not registering

--- Resources/Patches/improvements/asm/vaatiCredits.s
+++ Resources/Patches/improvements/asm/vaatiCredits.s
@@ -1,16 +1,16 @@
 .thumb
-push	{r4,r5,lr}
-mov	r4,r0
-ldrb	r0,[r4,#0x0E]
-sub	r0,#1
-strb	r0,[r4,#0x0E]
-push	{r0-r2}
 ldr	r0,=#0x3001002
 mov	r1,#4
 strb	r1,[r0]
 mov	r1,#0
 strb	r1,[r0,#1]
 strb	r1,[r0,#2]
-pop	{r0-r2}
-ldr	r3,=#0x8041D0B
+mov	r0,#0x51
+ldr	r3,=#0x807C728
+mov	lr,r3
+.short	0xF800
+ldr	r0,[r4,#0x64]
+ldr	r0,[r0,#8]
+str	r5,[r0,#0x64]
+ldr	r3,=#0x8041D1F
 bx	r3
