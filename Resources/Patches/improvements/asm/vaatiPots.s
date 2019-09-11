8218177ed0916b095057ef4bcddfc59d4398fda6
LeonarthCG <leonarth.cg@gmail.com>
2019-09-11 13:43:11 +0200

    vaati pot contents now match other regions

---
+++ Resources/Patches/improvements/asm/vaatiPots.s
@@ -0,0 +1,27 @@
+.equ pots1, pots
+.equ pots2, pots+4
+.thumb
+push	{lr}
+mov	r0,#0x77
+ldr	r3,=#0x807C5F4
+mov	lr,r3
+.short	0xF800
+cmp	r0,#0
+bne	vaati
+
+nuts:
+ldr	r0,pots1
+b	loadPots
+
+vaati:
+ldr	r0,pots2
+b	loadPots
+
+loadPots:
+ldr	r3,=#0x804AAF8
+mov	lr,r3
+.short	0xF800
+pop	{pc}
+.align
+.ltorg
+pots:
