6a948d9e9e820597041e66750c028bfe97c46498
LeonarthCG <leonarth.cg@gmail.com>
2019-09-13 20:09:54 +0200

    fix that weird lantern+boomerang glitch

--- Resources/Patches/improvements/asm/ocarinaGlitchFix.s
+++ Resources/Patches/improvements/asm/ocarinaGlitchFix.s
@@ -1,14 +1,30 @@
 .thumb
+@check if starting a cutscene
 ldr	r0,=#0x300116C
 ldrb	r1,[r0,#3]
 cmp	r1,#6
 bhs	end
+@and if ocarina
 cmp	r4,#0x17
 bne	notOcarina
 ldrb	r0,[r0]
 cmp	r0,#1
 bne	end
+
 notOcarina:
+@check if starting lantern
+ldr	r0,=#0x3000BE3
+ldrb	r1,[r0]
+cmp	r1,#0
+beq	noLantern
+@check if using bow or boomerang
+cmp	r4,#0x0C
+bhi	noLantern
+cmp	r4,#0x0B
+blo	noLantern
+b	end
+
+noLantern:
 mov	r0,r4
 ldr	r3,=#0x8077378
 mov	lr,r3
