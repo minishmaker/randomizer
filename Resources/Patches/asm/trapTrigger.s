01b881cc74ca863a25bf20ee7726dbe62eeaa8c6
LeonarthCG <leonarth.cg@gmail.com>
2019-09-24 18:09:22 +0200

    add trap damage back in

--- Resources/Patches/asm/trapTrigger.s
+++ Resources/Patches/asm/trapTrigger.s
@@ -42,10 +42,19 @@ swi	#6
 notRandom:
 ldr	r0,table
 lsl	r1,#3
+push	{r0,r1}
 ldr	r0,[r0,r1]
 cmp	r0,#0
 beq	end
 mov	lr,r0
+.short	0xF800
+pop	{r0,r1}
+add	r0,r1
+ldr	r1,[r0,#4]
+mov	r0,#0
+sub	r0,r1
+ldr	r3,=#0x80522BC
+mov	lr,r3
 .short	0xF800
 
 end:
