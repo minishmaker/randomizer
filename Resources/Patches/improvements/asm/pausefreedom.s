ddd9107104a6a58665eb3a9aaa41b9f298fa331a
LeonarthCG <leonarth.cg@gmail.com>
2019-11-01 20:54:08 +0100

    fix shrinking pause bug, maybe

---
+++ Resources/Patches/improvements/asm/pausefreedom.s
@@ -0,0 +1,25 @@
+.thumb
+@fade check
+ldr	r1,=#0x3000FD0
+ldr	r1,[r1]
+cmp	r1,#0
+bne	false
+
+cmp	r0,#0xA
+bhi	true
+
+vanilla:
+lsl	r0,#2
+ldr	r1,=#0x8070084
+ldr	r1,[r1]
+add	r0,r1
+ldr	r0,[r0]
+mov	pc,r0
+
+true:
+ldr	r3,=#0x80700B9
+bx	r3
+
+false:
+mov	r0,#0x4
+b	vanilla
