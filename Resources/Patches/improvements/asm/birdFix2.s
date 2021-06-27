e326aaccac23f7f7c7dd411eb66b1bf70dc78b4b
LeonarthCG <angel.cantos.giron@gmail.com>
2021-06-27 23:07:04 +0200

    fix some trap related entity graphics issues

--- Resources/Patches/improvements/asm/birdFix2.s
+++ Resources/Patches/improvements/asm/birdFix2.s
@@ -27,10 +27,11 @@ ldr	r3,=#0x80A2065
 bx	r3
 
 checkbird:
+ldrb	r1, [r4,#0x0A]
 cmp	r2,#0x95
 bne	vanilla
-ldrb	r1,[r4,#0x0E]
-cmp	r1,#0xFE
+ldrb	r3,[r4,#0x0E]
+cmp	r3,#0xFE
 blo	vanilla
 mov	r2,#0
 vanilla:
@@ -41,15 +42,18 @@ add	r2,r0
 
 @check if this is a trap
 push	{r2}
-ldrb	r1, [r4, #0x0A]
-cmp	r1, #0x1B
+ldrb	r3, [r4,#0x09]
+cmp	r3, #0x4D
+beq	notTrap
+ldrb	r3, [r4, #0x0A]
+cmp	r3, #0x1B
 bne	notTrap
-ldrb	r1, [r4,#0x08]
-cmp	r1, #0x06
+ldrb	r3, [r4,#0x08]
+cmp	r3, #0x06
 bne	notTrap
 mov	r0, r4
-ldr	r1, trapGetIcon
-mov	lr, r1
+ldr	r3, trapGetIcon
+mov	lr, r3
 .short	0xF800
 mov	r1, r0
 
