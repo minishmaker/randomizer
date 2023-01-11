33fd3b7cc154b27177713616568e0c4d84f6cb6c
Henny022 <hen.geheim22@gmail.com>
2023-01-11 04:05:33 +0100

    fix compiler breaking intrinsics with optimizations

--- RandomizerCore/Resources/Patches/c/intrinsics.c
+++ RandomizerCore/Resources/Patches/c/intrinsics.c
@@ -1,5 +1,22 @@
-void __aeabi_idivmod(int n, int d) {
-    (void)n;
-    (void)d;
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#define NAKED __attribute__((naked))
+
+int NAKED __aeabi_idiv(int n, int d) {
     asm("svc #6");
+    asm("bx lr");
+}
+
+unsigned int NAKED __aeabi_uidiv(unsigned int n, unsigned int d) {
+    asm("svc #6");
+    asm("bx lr");
+}
+
+int NAKED __aeabi_idivmod(int n, int d) {
+    asm("svc #6");
+    asm("bx lr");
+}
+
+unsigned int NAKED __aeabi_uidivmod(unsigned int n, unsigned int d) {
+    asm("svc #6");
+    asm("bx lr");
 }
