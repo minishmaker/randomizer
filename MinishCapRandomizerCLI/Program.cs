ab148b03f31d89049e9c3cb0b03d9688b8b9fd6d
Henny022 <git@henny022.de>
2023-02-13 00:25:41 +0100

    allow 0 for false when setting flag settings

--- MinishCapRandomizerCLI/Program.cs
+++ MinishCapRandomizerCLI/Program.cs
@@ -427,7 +427,7 @@ Exit                Exits the program
                 Console.WriteLine("2) Disabled");
                 Console.Write("Enter the number of the option to set the flag to: ");
                 var input = Console.ReadLine();
-                if (string.IsNullOrEmpty(input) || !int.TryParse(input, out var i) || (i != 1 && i != 2))
+                if (string.IsNullOrEmpty(input) || !int.TryParse(input, out var i) || (i != 0 && i != 1 && i != 2))
                 {
                     if (!input.Equals("exit", StringComparison.OrdinalIgnoreCase))
                     {
