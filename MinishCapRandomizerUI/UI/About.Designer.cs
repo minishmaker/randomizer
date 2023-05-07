38d0777634f52c6665e684ce1a9512658a7cef13
Hailey <haileyhable@gmail.com>
2023-05-06 19:48:45 -0500

    Finish about page

--- MinishCapRandomizerUI/UI/About.Designer.cs
+++ MinishCapRandomizerUI/UI/About.Designer.cs
@@ -48,6 +48,7 @@
 			// 
 			// pictureBox1
 			// 
+			this.pictureBox1.Image = global::MinishCapRandomizerUI.Properties.Resources.Untitled110;
 			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
 			this.pictureBox1.Name = "pictureBox1";
 			this.pictureBox1.Size = new System.Drawing.Size(130, 150);
